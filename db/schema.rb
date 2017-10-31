9e724fabb502c81e90e1b9a66981b8d0aa95d631
walidvb <walidvb@gmail.com>
2017-10-31 01:41:02 +0100

    styles modal and adds description

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171030195641) do
+ActiveRecord::Schema.define(version: 20171031003715) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,9 +32,10 @@ ActiveRecord::Schema.define(version: 20171030195641) do
   create_table "links", force: :cascade do |t|
     t.integer  "user_id"
     t.string   "url"
-    t.datetime "created_at",                null: false
-    t.datetime "updated_at",                null: false
-    t.text     "oembed",     default: "{}"
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
+    t.text     "oembed",      default: "{}"
+    t.text     "description"
   end
 
   create_table "taggings", force: :cascade do |t|

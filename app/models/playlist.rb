6d8f38aa2723f4a6180ff0948c8edb5c08c5407a
walidvb <walidvb@gmail.com>
2017-11-27 00:38:16 +0100

    adds friendly_id to playlists

--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -1,4 +1,7 @@
 class Playlist < ActiveRecord::Base
+  include FriendlyId
+  friendly_id :name, use: [:slugged, :finders]
+  
   belongs_to :user, inverse_of: :playlists
   has_many :playlist_assignments, inverse_of: :playlist
   has_many :links, through: :playlist_assignments, inverse_of: :playlists

bd760210641c256fe432b74afc2c5fad107cf072
walidvb <walidvb@gmail.com>
2017-12-30 06:39:33 +0100

    adds player

--- client/src/components/Link.js
+++ client/src/components/Link.js
@@ -7,40 +7,47 @@ import styles from './Link.scss';
 
 const Link = ({play, link }) => {
   const { thumbnail_url, title, width, height, provider, html } = link;
-  
+  let inner;
   if(link.playing){
-    return html;
+    inner = <div dangerouslySetInnerHTML={{
+      __html: html
+    }}/>;
   }
   else{
-    return (
-      <div onClick={() => play(link)} className="link item full-bg" style={{backgroundImage: `url(${thumbnail_url})`}}>
+    inner = (
+      <div>
         <div className={styles.thumbnail}>
           <img width="100%" height="400" src={thumbnail_url} />
         </div>
-          <div className={styles.infos}>
-            <h3 className={styles.title}>
-              { title }
-            </h3>
-            <div className={styles.top}>
-              <div className={styles.submitted}>
-                <i className="fa fa-user nouse-icon"></i>
-                XXX
-              </div>
+        <div className={styles.infos}>
+          <h3 className={styles.title}>
+            { title }
+          </h3>
+          <div className={styles.top}>
+            <div className={styles.submitted}>
+              <i className="fa fa-user nouse-icon"></i>
+              XXX
             </div>
-            <div className={styles.bottom}>
-              <em>
-                <i className="fa fa-tags nouse-icon"></i>
-                {  }
-              </em>
-              <div className={styles.controls}>
-                <a target="_blank" href="https://soundcloud.com/intimatesilence/intimate-silence-radio-016-juho-kusti"><i className="fa fa-soundcloud"></i></a>
-                <a href="#"><i className="fa fa-play"></i></a>
-              </div>
+          </div>
+          <div className={styles.bottom}>
+            <em>
+              <i className="fa fa-tags nouse-icon"></i>
+              {  }
+            </em>
+            <div className={styles.controls}>
+              <a target="_blank" href="https://soundcloud.com/intimatesilence/intimate-silence-radio-016-juho-kusti"><i className="fa fa-soundcloud"></i></a>
+              <a href="#"><i className="fa fa-play"></i></a>
             </div>
           </div>
         </div>
+      </div>
     );
   }
+  return (
+    <div onClick={() => play(link)} className="link item full-bg" style={{ backgroundImage: `url(${thumbnail_url})` }}>
+      {inner}
+    </div>
+  ); 
 };
 
 Link.propTypes = {

36ad579a37c8fe9fe5d6935bdc8ce2160d210d7b
walidvb <walidvb@gmail.com>
2017-12-30 02:23:20 +0100

    implement promises

--- client/src/actions/linkActions.js
+++ client/src/actions/linkActions.js
@@ -1,9 +1,15 @@
 import * as types from './actionTypes';
+import request from '../request';
+import routes from '../routes';
 
 export const getLinks = (filters) => {
   return {
     type: types.GET_LINKS,
     filters,
-    payload: 2,
+    payload: new Promise(resolve => {
+      request(routes.links.index)
+        .then(response => resolve(response.json()))
+        .catch(error => console.log(error))
+    }),
   };
 }
--- client/src/reducers/linkReducers.js
+++ client/src/reducers/linkReducers.js
@@ -5,13 +5,24 @@ const initialState = {
 }
 
 export default (state = initialState, action) => {
-  console.log(state);
   switch (action.type) {
     case types.GET_LINKS:
       return {
         ...state,
-        links: action.links,
+        list: action.payload,
       };
+    case `${types.GET_LINKS}_PENDING`:
+      return {
+        ...state,
+        loading: true,
+      }
+    case `${types.GET_LINKS}_REJECTED`:
+    case `${types.GET_LINKS}_FULFILLED`:
+    return {
+      ...state,
+      list: action.payload,
+      loading: false
+    }
     default:
       return state;
   }

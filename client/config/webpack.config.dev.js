16d6caa3195165410a85ffe4ac70c9a26469d383
walidvb <walidvb@gmail.com>
2018-01-02 19:46:45 +0100

    adds css hot reload

--- client/config/webpack.config.dev.js
+++ client/config/webpack.config.dev.js
@@ -188,58 +188,33 @@ module.exports = {
           },
           {
             test: /\.css$/,
-            use: ExtractTextPlugin.extract({
-              fallback: 'style-loader',
-              use: [
-                {
-                  loader: 'css-loader',
-                  options: {
-                    modules: true,
-                    localIdentName: '[name]__[local]___[hash:base64:5]'
-                  }
-                },
-                'postcss-loader'
-                // {
-                //   loader: require.resolve('postcss-loader'),
-                //   options: {
-                //     // Necessary for external CSS imports to work
-                //     // https://github.com/facebookincubator/create-react-app/issues/2677
-                //     ident: 'postcss',
-                //     plugins: () => [
-                //       require('postcss-flexbugs-fixes'),
-                //       autoprefixer({
-                //         browsers: [
-                //           '>1%',
-                //           'last 4 versions',
-                //           'Firefox ESR',
-                //           'not ie < 9', // React doesn't support IE8 anyway
-                //         ],
-                //         flexbox: 'no-2009',
-                //       }),
-                //     ],
-                //   },
-                // },
-              ]
-            }),
+            use: [
+              'style-loader',
+              {
+                loader: 'css-loader',
+                query: {
+                  modules: true,
+                  localIdentName: '[name]__[local]___[hash:base64:5]'
+                }
+              },
+              'postcss-loader'
+            ]
           },
-          // WALID
           {
-            test: /\.scss/,
-            use: ExtractTextPlugin.extract({
-              fallback: 'style-loader',
-              use: [
-                {
-                  loader: 'css-loader',
-                  options: {
-                    modules: true,
-                    sourceMap: true,
-                    importLoaders: 2,
-                    localIdentName: '[name]__[local]___[hash:base64:5]'
-                  }
-                },
-                'sass-loader'
-              ]
-            }),
+            test: /\.scss$/,
+            use: [
+              'style-loader',
+              {
+                loader: 'css-loader',
+                query: {
+                  modules: true,
+                  sourceMap: true,
+                  importLoaders: 2,
+                  localIdentName: '[name]__[local]___[hash:base64:5]'
+                }
+              },
+              'sass-loader'
+            ]
           },
           // "file" loader makes sure those assets get served by WebpackDevServer.
           // When you `import` an asset, you get its (virtual) filename.

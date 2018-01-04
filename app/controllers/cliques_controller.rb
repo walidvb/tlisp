6c1a9fda976cd47f91134f60f74e490f11379195
walidvb <walidvb@gmail.com>
2018-01-04 05:35:24 +0100

    progress bar and unique links to join clique

--- app/controllers/cliques_controller.rb
+++ app/controllers/cliques_controller.rb
@@ -5,7 +5,7 @@ class CliquesController < ApplicationController
 
   def join
     @inviter = params[:by]
-    @links = @clique.links
+    @links = @clique.links.uniq
     session[:join_clique_id] = @clique.id
   end
 

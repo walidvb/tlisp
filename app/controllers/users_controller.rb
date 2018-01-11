21f42816fe4671c86dc8babdcea5c23641f7754a
walidvb <walidvb@gmail.com>
2018-01-11 12:02:14 +0100

    rm users with no names

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,8 @@ class UsersController < ApplicationController
   def index
     @users = current_user.cliques.map(&:users).flatten.uniq.select{|u| u.id != current_user.id}
 
+    # remove users with no names or initials as this endoint is currently only used for mentioning at the moment
+    @users = @users.select{|u| !(u.initials.blank? && u.name.blank?)}
     render json: {
         users: @users,
     }

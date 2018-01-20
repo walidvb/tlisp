0bc315fb39fe1c7b652ab7620f139eb99102d1e7
walidvb <walidvb@gmail.com>
2018-01-20 03:42:25 +0100

    mobile landing page

--- client/src/components/static/NewsletterPage.js
+++ client/src/components/static/NewsletterPage.js
@@ -3,54 +3,44 @@ import PropTypes from 'prop-types'
 
 import { request, routes } from '../../request';
 
+import ScrollingList from './ScrollingList';
+
 import styles from './NewsletterPage.scss';
 import listStyles from '../links/LinkList.scss';
-
 export default class NewsletterPage extends Component {
     static propTypes = {
 
     }
     state = {
-        loading: true,
-        covers: []
     }
-    componentDidMount(){
-        request(routes.api.covers)
-        .then(({ covers }) => {
-            this.setState({ covers, loading: false });
-            this.startScrolling();
-        })
-    }
-    startScrolling(){
-        console.log(this.coversContainer.scrollHeight)
-        var direction = 1;
-        const scrollTo = () => {
-            var newY = this.coversContainer.scrollTop + direction;
-            this.coversContainer.scrollTop = newY;
-            if (newY >= this.coversContainer.scrollHeight - window.innerHeight || newY <= 0) {
-                direction = -direction;
-            }
-        }
-        this.coversContainer.scrollTop = this.coversContainer.scrollHeight - window.innerHeight;
-        setInterval(scrollTo, 50);
-    }
-    renderBackground(){
-        const { covers, loading } = this.state;
-        return <div 
-            ref={(coversContainer) => {this.coversContainer = coversContainer}}
-            className={[listStyles.container__grid, styles.links_container,].join(' ')}>
-                {covers.map((url, i) => (
-                    <div key={i} className={[listStyles.item__grid, listStyles.no__spacing, styles.thumb].join(' ')} >
-                        <div><img src={url} /> </div>
-                    </div>))}
-            </div>
+    renderForm(){
+        return "THE FORM";
     }
     render() {
-        const { covers, loading} = this.state;
         return (
             <div>
-                {this.renderBackground()}
-                <h1>DIGGERS DELIGHTS</h1>
+                <ScrollingList />
+                <h3 className={styles.container_header}>Welcome back to digging 1.0</h3>
+                <div className={styles.container}>
+                    <div className={styles.slogan}>By friends, for friends</div>
+                    <div className={styles.center}>
+                        <h1 className={styles.site_name}><div className={styles.top}>DIGGERS'</div> DELIGHTS</h1>
+                        {this.renderForm()}
+                    </div>
+                    {/* <div className={styles.supported}>
+                        <div className={styles.title}>
+                            Supported Services
+                        </div>
+                        <ul>
+                            <li>Soundcloud</li>
+                            <li>Bandcamp</li>
+                            <li>Youtube</li>
+                            <li>Mixcloud</li>
+                            <li>And many more...</li>
+                        </ul>
+                    </div> */}
+                </div>
+                <div className={styles.container_header} style={{textAlign: 'right'}}>By a friend</div>
             </div>
         )
     }

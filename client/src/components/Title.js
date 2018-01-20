a19379cc7732afa3bf208e88259ac78ee1bc8f1d
walidvb <walidvb@gmail.com>
2018-01-20 04:34:14 +0100

    adds animations

--- client/src/components/Title.js
+++ client/src/components/Title.js
@@ -2,7 +2,10 @@ import React from 'react'
 import styles from './Title.scss'   
 function Title() {
     return (
-        <h1 className={styles.site_name}><div className={styles.top}>DIGGERS'</div> DELIGHTS</h1>
+        <h1 className={styles.site_name}>
+            <div className={styles.top}>DI<span>G</span><span>G</span>ERS'</div> 
+            DELIGHTS
+        </h1>
     )
 }
 

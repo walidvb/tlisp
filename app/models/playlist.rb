2c280bfa42565e2141876863ba7e863ac5c8f4b9
walidvb <walidvb@gmail.com>
2018-01-27 00:43:36 -0200

    adds dependencies

--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -3,7 +3,7 @@ class Playlist < ActiveRecord::Base
   friendly_id :name, use: [:slugged, :finders]
   
   belongs_to :user, inverse_of: :playlists
-  has_many :playlist_assignments, inverse_of: :playlist
+  has_many :playlist_assignments, inverse_of: :playlist, :dependent => :destroy
   has_many :links, through: :playlist_assignments, inverse_of: :playlists
 
   validates_presence_of :name

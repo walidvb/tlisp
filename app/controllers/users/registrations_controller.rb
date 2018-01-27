3732e53f0af76b6552174e0b6ecb90ae09934962
walidvb <walidvb@gmail.com>
2018-01-27 00:52:31 -0200

    fixes tests

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -31,12 +31,12 @@ class Users::RegistrationsController < Devise::RegistrationsController
       else
         set_flash_message! :notice, :"signed_up_but_#{resource.inactive_message}"
         expire_data_after_sign_in!
-        # respond_with resource, location: after_inactive_sign_up_path_for(resource)
+        respond_with resource, location: after_inactive_sign_up_path_for(resource)
       end
     else
       clean_up_passwords resource
       set_minimum_password_length
-      # respond_with resource
+      respond_with resource
     end
   end
 

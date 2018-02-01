438bfaed757614bf2075ad596695ea54530efd02
walidvb <walidvb@gmail.com>
2018-02-01 12:44:40 -0200

    rm code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -75,8 +75,6 @@ class ApplicationController < ActionController::Base
   end
 
   def log_to_slack options = {}
-        emoji = %w{🌴 🏖 👍 🤘 🎉 ✌🏻 👌 🤷‍♂️ 💫 🔥 🌈 📻 🛀🏿}.sample
-      
-        Slack.post! ENV['DD_SLACK_WEBHOOK_URL'], options if Rails.env.production?
+    Slack.post! ENV['DD_SLACK_WEBHOOK_URL'], options if Rails.env.production?
   end
 end

9a6cbcea382190e66a52da9951798791fcaf1ea3
walidvb <walidvb@gmail.com>
2018-02-18 01:49:17 -0300

    npm i

--- client/src/components/links/LinksContainer.js
+++ client/src/components/links/LinksContainer.js
@@ -3,7 +3,6 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { withRouter } from 'react-router-dom'
-import InfiniteScroll from 'react-infinite-scroller';
 
 import routes from '../../routes.js';
 import request from '../../request.js';

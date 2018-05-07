dcfdfcd17140fa11cb130f56860c8ac2935b76a3
walidvb <walidvb@gmail.com>
2018-05-07 18:16:31 +0200

    fixes reset password page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -36,7 +36,8 @@ class ApplicationController < ActionController::Base
 
   # Redirects on successful sign in
   def after_sign_in_path_for(resource)
-    resource.admin? ? rails_admin.dashboard_path : root_path
+    # resource.admin? ? rails_admin.dashboard_path : root_path
+    root_path
   end
 
   # Auto-sign out locked users

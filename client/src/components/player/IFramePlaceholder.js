8f7fcd94a950557b5342a8eeb296bfc9cb57e19f
walidvb <walidvb@gmail.com>
2018-10-08 20:30:17 +0200

    fixes resize

--- client/src/components/player/IFramePlaceholder.js
+++ client/src/components/player/IFramePlaceholder.js
@@ -1,22 +1,25 @@
 import React, { Component } from 'react'
 
 export default class IFramePlaceholder extends Component {
-  placeTarget(off){
+  rePlace = this.rePlace.bind(this)
+  placeTarget(off = false, placeTop = true){
     const { left, top, width, height } = this.container.getBoundingClientRect();
     const target = document.getElementById('player-placeholder');
     target.style.position = 'fixed';
-    target.style.top = off ? '-200%' : `${top}px`;
+    target.style.top = off && placeTop ? '-200%' : `${top}px`;
     target.style.left = left + 'px';
     target.style.width = width+'px';
   }
-  // componentWillReceiveProps(){
-  //   this.placeTarget();
-  // }
+  rePlace(){
+    this.placeTarget(true, false)
+  }
   componentDidMount( ){
     this.placeTarget();
+    window.addEventListener('resize', this.rePlace, true) 
   }
   componentWillUnmount(){
     this.placeTarget(true)
+    window.removeEventListener('resize', this.rePlace, true)
   }
   render() {
     return (

d92b751b9c5ff7598b1a24bfc5cbf173bdeac37a
Walid <hello@walidvb.com>
2019-03-17 13:35:46 +0100

    better cliques UI

--- client/src/components/cliques/CliquesList.js
+++ client/src/components/cliques/CliquesList.js
@@ -5,13 +5,14 @@ function CliquesList(props) {
   const { cliques, clickName, clickUser } = props;
   const { users } = props.filters;
   const filteredUserIds = users.map(e => e.id)
+  const filteredCliqueIds = cliques.map(e => e.id)
   return (
     <div>
       {cliques.map(clique => {
         const users = clique.users.sort((a, b) => b.link_clique_assignments_count - a.link_clique_assignments_count)
-        const isActive = cliques && cliques.map(e => e.id).includes(clique.id)
+        const isActive = cliques && filteredCliqueIds.includes(clique.id)
         return <div key={clique.id} className={[styles.wrapper, isActive ? styles.active : ''].join(' ')}>
-          <div onClick={() => clickName(clique)}>{clique.name}</div>
+          <div className={styles.cliqueName} onClick={() => clickName(clique)}>{clique.name}</div>
           <div className={styles.users}>
             {users.map(renderUser)}
           </div>
@@ -30,6 +31,7 @@ function CliquesList(props) {
     ].join(' ')} key={user.id}
       onClick={() => hasLinks && clickUser(user)}>
       {user.name}
+      <span class={styles.badge}>{user.link_clique_assignments_count}</span>
     </div>
   }
 }

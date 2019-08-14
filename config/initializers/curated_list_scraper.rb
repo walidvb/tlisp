8e58475b3d2bf8f554b95ff194cdab14951289ab
Walid <hello@walidvb.com>
2019-08-13 21:41:39 -0300

    fix for fb and other unrelated iframes

--- config/initializers/curated_list_scraper.rb
+++ config/initializers/curated_list_scraper.rb
@@ -1,8 +1,9 @@
 class CuratedListScraper < Scraper
   def get_iframes
-    @page.search('iframe').map do |iframe|
-      puts iframe
+    @page.search('iframe[src]').map do |iframe|
       iframe.attributes['src'].value
-    end
+    end.select do |src|
+      !/faebook\.com|twitter\.com|googletagmanager.com|google.com/.match(src)
+    end.compact
   end
 end

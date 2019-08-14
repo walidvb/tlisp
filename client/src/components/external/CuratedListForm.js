f05d28cce6fbb21475e4309280e994816a5b4471
Walid <hello@walidvb.com>
2019-08-13 21:25:41 -0300

    change to url

--- client/src/components/external/CuratedListForm.js
+++ client/src/components/external/CuratedListForm.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 
 import styles from '../static/NewsletterForm.scss'
+import { Link } from 'react-router-dom';
 
 
 const CuratedListForm = ({}) => {
@@ -8,7 +9,7 @@ const CuratedListForm = ({}) => {
 
   return <div className={styles.container}>
     <input placeholder="Paste a link here to see the page as a playlist" type="text" className={styles.input} value={url} onChange={({target: { value } } ) => setUrl(value)}/>
-    <a href={`/curated?source=${encodeURIComponent(url)}`}> See all embeds</a>
+    <Link to={`/curated?url=${encodeURIComponent(url)}`}> See all embeds</Link>
   </div>
 }
 

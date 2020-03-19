bd3544cfd6a0e393a95f20924cba7ff361510cd1
Walid <hello@walidvb.com>
2020-03-19 01:33:22 -0300

    hello react

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -14,5 +14,5 @@
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
-alert('loadinng');
-console.log('Hello World from Webpacker');
+
+import './hello_react';

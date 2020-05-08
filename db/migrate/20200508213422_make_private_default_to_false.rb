58949875ad6d697ae4b9e65ead90a609f41f10f3
Walid <hello@walidvb.com>
2020-05-08 18:41:17 -0300

    rm migration

--- db/migrate/20200508213422_make_private_default_to_false.rb
+++ db/migrate/20200508213422_make_private_default_to_false.rb
@@ -1,6 +1,5 @@
 class MakePrivateDefaultToFalse < ActiveRecord::Migration[5.2]
   def change
     change_column :curated_lists, :private, :boolean, default: true, index: true
-    CuratedList.where(private: nil).update_columns(private: false)
   end
 end

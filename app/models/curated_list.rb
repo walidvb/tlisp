9f2f5adb45d9dc14bbec73cc9882591f969d352d
Walid <hello@walidvb.com>
2020-05-08 15:54:38 -0300

    rename public to publci_

--- app/models/curated_list.rb
+++ app/models/curated_list.rb
@@ -2,5 +2,5 @@ class CuratedList < ActiveRecord::Base
   has_many :curated_list_links, dependent: :destroy
   has_many :links, through: :curated_list_links
 
-  scope :public, ->{ where.not(private: true)}
+  scope :public_, ->{ where.not(private: true)}
 end

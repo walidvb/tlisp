d9730c725d3b647875a015515d07c3e071079e6c
Walid <hello@walidvb.com>
2020-09-17 14:18:09 +0200

    prevent double redirect

--- app/controllers/curated_lists_controller.rb
+++ app/controllers/curated_lists_controller.rb
@@ -35,7 +35,6 @@ class CuratedListsController < ApplicationController
     sources = params[:sources]
     create_or_show sources
     if @error
-      render plain: 'error', content_type: 'text/plain'
     else
       redirect_to "/curated/#{@curated_list.id}"
     end
@@ -86,8 +85,8 @@ class CuratedListsController < ApplicationController
       headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, DELETE, OPTIONS'
       headers['Access-Control-Allow-Headers'] = 'X-Requested-With, X-Prototype-Version, Token'
       headers['Access-Control-Max-Age'] = '1728000'
-
-      render :text => '', :content_type => 'text/plain'
+      puts "cors_preflight_check"
+      head :ok
     end
   end
 end
